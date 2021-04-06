5d9fb1f9d15b17df44021d9517af0def8d4ceefc
Martin Sichibeya <sichibeyam@gmail.com>
2021-04-06 09:21:01 -0600

    first commit

---
+++ sample.js
@@ -0,0 +1,517 @@
+const arry = [
+    {
+        img: "d1.jpg",
+        title: "Bloodshot",
+        rate: 9.8,
+        type: "action",
+        overview: "This is an action movie"
+    },
+    {
+        img: "d2.jpg",
+        title: "The Hero",
+        rate: 4.8,
+        type: "action",
+        overview: "This is an action movie"
+    },
+    {
+        img: "d3.jpg",
+        title: "Wars",
+        rate: 7.0,
+        type: "action",
+        overview: "This is an action movie"
+    },
+    {
+        img: "d4.jpg",
+        title: "Love",
+        rate: 9.2,
+        type: "romance",
+        overview: "This is a romantic movie"
+    },
+    {
+        img: "d5.jpg",
+        title: "MALANG",
+        rate: 6.1,
+        type: "action",
+        overview: "This is an action movie"
+    },
+    {
+        img: "d6.jpg",
+        title: "Me Before You",
+        rate: 10,
+        type: "romance",
+        overview: "This is a romantic movie"
+    },
+    {
+        img: "d7.jpg",
+        title: "The Original",
+        rate: 9.9,
+        type: "serie",
+        overview: "This is a series"
+    },
+    {
+        img: "d8.jpg",
+        title: "BIGIL",
+        rate: 4.9,
+        type: "action",
+        overview: "This is an action movie"
+    },
+    {
+        img: "d9.jpg",
+        title: "John Wick 2",
+        rate: 10,
+        type: "action",
+        overview: "This is an action movie"
+    },
+    {
+        img: "d10.jpg",
+        title: "For The Love of JESSE",
+        rate: 6.8,
+        type: "romance",
+        overview: "This is a romantic movie"
+    },
+    {
+        img: "d11.jpg",
+        title: "THOR",
+        rate: 9.1,
+        type: "action",
+        overview: "This is an action movie"
+    },
+    {
+        img: "d12.jpg",
+        title: "FROZEN",
+        rate: 7.8,
+        type: "animation",
+        overview: "This is an animation"
+    },
+    {
+        img: "d13.jpg",
+        title: "CRIMINAL",
+        rate: 9.6,
+        type: "action",
+        overview: "This is an action movie"
+    },
+    {
+        img: "d14.jpg",
+        title: "AVENGERS endgame",
+        rate: 10.0,
+        type: "action",
+        overview: "This is an action movie"
+    },
+    {
+        img: "d15.jpg",
+        title: "PRISON BREAK 4",
+        rate: 9.8,
+        type: "serie",
+        overview: "This is a series"
+    },
+    {
+        img: "d16.jpg",
+        title: "GAME OF THRONES",
+        rate: 10.0,
+        type: "serie",
+        overview: "This is a series"
+    },
+    {
+        img: "d17.jpg",
+        title: "HACKED",
+        rate: 8.8,
+        type: "romance",
+        overview: "This is a romantic movie"
+    },
+    {
+        img: "d18.jpg",
+        title: "BRAVE",
+        rate: 8.0,
+        type: "animation",
+        overview: "This is an animation "
+    },
+    {
+        img: "d19.jpg",
+        title: "LOVESTORY",
+        rate: 5.8,
+        type: "romance",
+        overview: "This is a romantic movie"
+    },
+    {
+        img: "d20.jpg",
+        title: "HOTEL TRANSYLVANIA 2",
+        rate: 8.9,
+        type: "animation",
+        overview: "This is an animation"
+    },
+    {
+        img: "d21.jpg",
+        title: "The Little Things",
+        rate: 7.0,
+        type: "romance",
+        overview: "This is a romantic movie"
+    },
+    {
+        img: "d22.jpg",
+        title: "POISON 2",
+        rate: 1.7,
+        type: "romance",
+        overview: "This is a romantic movie"
+    },
+    {
+        img: "d23.jpg",
+        title: "First Look Tomorrow",
+        rate: 3.1,
+        type: "romance",
+        overview: "This is a romantic movie"
+    },
+    {
+        img: "d24.jpg",
+        title: "John Carter",
+        rate: 6.8,
+        type: "action",
+        overview: "This is an action movie"
+    },
+    {
+        img: "d25.jpg",
+        title: "THE CRAODS",
+        rate: 7.3,
+        type: "animation",
+        overview: "This is an animation movie"
+    },
+    {
+        img: "d26.jpg",
+        title: "BHOOT",
+        rate: 7.8,
+        type: "romance",
+        overview: "This is a romantic movie"
+    },
+    {
+        img: "d27.jpg",
+        title: "AQUAMAN",
+        rate: 9.8,
+        type: "action",
+        overview: "This is an action movie"
+    },
+    {
+        img: "d28.jpg",
+        title: "Street Fighter 2",
+        rate: 6.8,
+        type: "animation",
+        overview: "This is an animation movie"
+    },
+    {
+        img: "d29.jpg",
+        title: "VILLAIN",
+        rate: 9.2,
+        type: "romance",
+        overview: "This is a romantic movie"
+    },
+    {
+        img: "d30.jpg",
+        title: "MEGAMIND",
+        rate: 9.8,
+        type: "animation",
+        overview: "This is an animation movie"
+    },
+    {
+        img: "d31.jpg",
+        title: "Brothers",
+        rate: 10,
+        type: "action",
+        overview: "This is an action movie"
+    },
+    {
+        img: "d32.jpg",
+        title: "GOT",
+        rate: 10.0,
+        type: "serie",
+        overview: "This is a series"
+    },
+    {
+        img: "d33.jpg",
+        title: "Street Dancer",
+        rate: 9.9,
+        type: "romance",
+        overview: "This is a romantic movie"
+    },
+    {
+        img: "d34.jpg",
+        title: "Ni Nandar",
+        rate: 5.8,
+        type: "romance",
+        overview: "This is a romantic movie"
+    },
+    {
+        img: "d35.jpg",
+        title: "ICE AGE",
+        rate: 8.8,
+        type: "animation",
+        overview: "This is an animation movie"
+    },
+    {
+        img: "d36.jpg",
+        title: "WAR",
+        rate: 6.9,
+        type: "action",
+        overview: "This is an action movie"
+    },
+    {
+        img: "d37.jpg",
+        title: "Prison Break 3",
+        rate: 8.8,
+        type: "serie",
+        overview: "This is a series"
+    },
+    {
+        img: "d38.jpg",
+        title: "ASUR",
+        rate: 10,
+        type: "romance",
+        overview: "This is a romantic movie"
+    },
+    {
+        img: "d39.jpg",
+        title: "SHERLOCK",
+        rate: 8.1,
+        type: "romance",
+        overview: "This is a romantic movie"
+    },
+    {
+        img: "d40.jpg",
+        title: "ANTS",
+        rate: 2.9,
+        type: "animation",
+        overview: "This is an animation movie"
+    }
+];
+
+const trendArry = [
+    {
+        img: 'USA.jpg',
+        title: 'Coming to America',
+        rate: 9.9
+    },
+    {
+        img: 'd1.jpg',
+        title: 'Bloodshot',
+        rate: 10.0
+    },
+    {
+        img: 'd32.jpg',
+        title: 'Game of Thrones',
+        rate: 9.5
+    },
+    {
+        img: 'd27.jpg',
+        title: 'AQUAMAN',
+        rate: 8.2
+    }
+];
+
+const trendImage = document.getElementById("img");
+const trendTitle = document.getElementById("trend-title");
+const trendRate = document.getElementById("trend-rate");
+
+
+
+const container = document.querySelector(".all");
+const homeBtn = document.querySelector(".home");
+const typeBtn = document.querySelector(".type");
+const typeShow = document.querySelector(".type-menu");
+const ratingBtn = document.querySelector(".rating");
+const ratingShow = document.querySelector(".rating-menu");
+
+function active(elem) {
+    elem.classList.add("active");
+
+}
+
+function inactive(elemArry) {
+    elemArry.forEach(function (e) {
+        e.classList.remove("active");
+    });
+}
+
+typeBtn.addEventListener("click", function () {
+    active(typeBtn)
+    inactive([ratingBtn])
+    typeShow.style.visibility = "visible";
+    ratingShow.style.visibility = "hidden";
+});
+
+ratingBtn.addEventListener("click", function () {
+    active(ratingBtn)
+    inactive([typeBtn])
+    typeShow.style.visibility = "hidden";
+    ratingShow.style.visibility = "visible";
+});
+
+// type part
+const actionBtn = document.querySelector(".action");
+const animationBtn = document.querySelector(".animation");
+const romanceBtn = document.querySelector(".romance");
+const serieBtn = document.querySelector(".serie");
+
+actionBtn.addEventListener("click", function () {
+    let type = arry.filter(function (item) {
+        if (item.type === "action") {
+            typeShow.style.visibility = "hidden";
+            return item;
+        }
+
+    });
+    showMovies(type);
+});
+
+
+homeBtn.addEventListener("click", function () {
+    typeShow.style.visibility = "hidden";
+
+    container.innerHTML = `
+    
+    <div class="outter-head">
+    <h3>Trending Movies</h3>
+</div>
+<div class="outter-main">
+    <div class="inner-main">
+        <!-- First trend  -->
+        <div class="main">
+            <img src="USA.jpg" id="img" />
+            <div class="sub-head">
+                <div class="upper">
+                    <h6 id="trend-title">comming to America 2</h6>
+                    <h5 id="trend-rate">9.9</h5>
+                </div>
+                <h5 class="main-view">Overview</h5>
+            </div>
+        </div>
+    </div>`;
+
+});
+
+
+let trendStatus = 0;
+
+setInterval(function () {
+
+    trendStatus++;
+
+    if (trendStatus > trendArry.length - 1) {
+        trendStatus = 0;
+    }
+    trendImage.src = trendArry[trendStatus].img;
+    trendTitle.innerHTML = trendArry[trendStatus].title;
+    trendRate.innerHTML = trendArry[trendStatus].rate;
+
+}, 6000);
+
+animationBtn.addEventListener("click", function () {
+    let type = arry.filter(function (item) {
+        if (item.type === "animation") {
+            typeShow.style.visibility = "hidden";
+            return item;
+        }
+
+    });
+    showMovies(type);
+});
+
+romanceBtn.addEventListener("click", function () {
+    let type = arry.filter(function (item) {
+        if (item.type === "romance") {
+            typeShow.style.visibility = "hidden";
+            return item;
+        }
+
+    });
+    showMovies(type);
+});
+
+serieBtn.addEventListener("click", function () {
+    let type = arry.filter(function (item) {
+        if (item.type === "serie") {
+            typeShow.style.visibility = "hidden";
+            return item;
+        }
+
+    });
+    showMovies(type);
+});
+
+// rating part 
+const highBtn = document.querySelector(".high");
+const mediumBtn = document.querySelector(".medium");
+const lowBtn = document.querySelector(".low");
+
+highBtn.addEventListener("click", function () {
+    let type = arry.filter(function (item) {
+        if (item.rate >= 8) {
+            ratingShow.style.visibility = "hidden";
+            return item;
+        }
+
+    });
+    showMovies(type);
+});
+
+mediumBtn.addEventListener("click", function () {
+    let type = arry.filter(function (item) {
+        if (item.rate >= 5 && item.rate < 8) {
+            ratingShow.style.visibility = "hidden";
+            return item;
+        }
+
+    });
+    showMovies(type);
+});
+
+lowBtn.addEventListener("click", function () {
+    let type = arry.filter(function (item) {
+        if (item.rate < 5) {
+            ratingShow.style.visibility = "hidden";
+            return item;
+        }
+
+    });
+    showMovies(type);
+});
+
+
+const overviewBtn = document.getElementById("overview");
+const overview = document.querySelector(".display-text");
+const cancelButton = document.getElementById("cancel-view");
+
+
+
+function showMovies(menu) {
+    let x = menu.map(function (item) {
+
+        return '<div class="outter">' +
+            '<div class="cover">' +
+            '<img src= ' + item.img + ' alt="Movie 1" class="img" />' +
+            '<div class="main-all">' +
+            '<div class="head">' +
+            '<h6 class="title">' + item.title + '</h6>' +
+            '<h6 class="rate">' + item.rate + '</h6>' +
+            '</div>' +
+            '<h5 id="overview">Overview</h5>' +
+            '</div>' +
+            '</div>' +
+            '</div>';
+
+    });
+    let y = x.join("");
+    container.innerHTML = y;
+
+}
+
+const screen = document.querySelector(".screen");
+const home = document.querySelector(".outter-1");
+
+screen.addEventListener("keyup", function () {
+    let show = screen.value.toUpperCase();
+    let type = arry.filter(function (item) {
+        if (item.title.toUpperCase().indexOf(show) > -1) {
+            return item;
+        }
+
+
+    });
+    showMovies(type);
+});
